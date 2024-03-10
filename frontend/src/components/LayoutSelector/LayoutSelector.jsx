51cd8b6b9027edd728a19025bd91a3e6a27fe1e1
Thao <thao.tranthao0611@hcmut.edu.vn>
2024-03-10 17:26:38 +0700

    add pagination and layout selector

---
+++ frontend/src/components/LayoutSelector/LayoutSelector.jsx
@@ -0,0 +1,31 @@
+import './LayoutSelector.css'; 
+import { TfiLayoutGrid2 } from "react-icons/tfi"; 
+import { TfiLayoutListThumb } from "react-icons/tfi"; 
+import React from 'react'; 
+ 
+const LayoutSelector = ({ onChange, selectedLayout }) => { 
+  return ( 
+    <div class = 'selector'> 
+      <label> 
+        <input 
+          type="radio" 
+          value="list" 
+          checked={selectedLayout === 'list'} 
+          onChange={() => onChange('list')} 
+        /> 
+        <div class = 'icon'> <TfiLayoutListThumb/> </div> 
+      </label> 
+      <label> 
+        <input 
+          type="radio" 
+          value="grid" 
+          checked={selectedLayout === 'grid'} 
+          onChange={() => onChange('grid')} 
+        /> 
+        <div class = 'icon'> <TfiLayoutGrid2/> </div> 
+      </label> 
+    </div> 
+  ); 
+}; 
+ 
+export default LayoutSelector;
