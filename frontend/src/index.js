f33766e673ed466ba86ec45ec98f16df570da86b
Thao Tran <thao.tran@nois.vn>
2024-03-12 17:38:07 +0700

    Patient Info

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,19 +5,26 @@ import { ChakraProvider } from "@chakra-ui/react";
 import SideBar from "./components/Sidebar/Sidebar";
 import PatientList from "./components/PatientList/PatientList";
 import Note from "./components/Note/Note";
+import LayoutSelector from "./components/LayoutSelector/LayoutSelector";
 import InformationTag from "./components/InformationTag/InformationTag";
 import OveralTag from "./components/OveralTag/OveralTag";
 import LoginPage from "./screens/Login/LoginPage";
+import MyTabs from "./components/Tabs/Tabs";
+import PatientInfor from "./components/PatientInfor/PatientInfor";
+
 function App() {
   
   return (
     <ChakraProvider>
+      {/* <LoginPage/> */}
       {/* <PatientList /> */}
       {/* <SideBar /> */}
       {/* <Note /> */}
       {/* <InformationTag/> */}
       {/* <OveralTag /> */}
-      <LoginPage/>
+      {/* <LayoutSelector/> */}
+      {/* <MyTabs/> */}
+      {<PatientInfor/>}
       {/* <InputTag/> */}
     </ChakraProvider>
   )
