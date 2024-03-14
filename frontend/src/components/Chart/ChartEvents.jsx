cb333fb9ed8fa57f0f6aeb7c31c09779408b8af1
anhducduonghuynh <duc.duong122@hcmut.edu.vn>
2024-03-14 15:10:10 +0700

    upload chart component

---
+++ frontend/src/components/Chart/ChartEvents.jsx
@@ -0,0 +1,125 @@
+import React, { useRef } from 'react';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+} from 'chart.js';
+import {
+  Chart,
+  getDatasetAtEvent,
+  getElementAtEvent,
+  getElementsAtEvent,
+} from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip
+);
+
+const options = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      type: 'line',
+      label: 'Dataset 1',
+      borderColor: 'rgb(255, 99, 132)',
+      borderWidth: 2,
+      fill: false,
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+    },
+    {
+      type: 'line',
+      label: 'Dataset 4',
+      borderColor: 'rgb(255, 99, 0)',
+      borderWidth: 2,
+      fill: false,
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+    },
+    {
+      type: 'bar',
+      label: 'Dataset 2',
+      backgroundColor: 'rgb(75, 192, 192)',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'white',
+      borderWidth: 2,
+    },
+    {
+      type: 'bar',
+      label: 'Dataset 3',
+      backgroundColor: 'rgb(53, 162, 235)',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+    },
+    
+  ],
+};
+
+const ChartEvents = () => {
+  const printDatasetAtEvent = (dataset) => {
+    if (!dataset.length) return;
+
+    const datasetIndex = dataset[0].datasetIndex;
+
+    console.log(data.datasets[datasetIndex].label);
+  };
+
+  const printElementAtEvent = (element) => {
+    if (!element.length) return;
+
+    const { datasetIndex, index } = element[0];
+
+    console.log(data.labels[index], data.datasets[datasetIndex].data[index]);
+  };
+
+  const printElementsAtEvent = (elements) => {
+    if (!elements.length) return;
+
+    console.log(elements.length);
+  };
+
+  const chartRef = useRef(null);
+
+  const onClick = (event) => {
+    const { current: chart } = chartRef;
+
+    if (!chart) {
+      return;
+    }
+
+    printDatasetAtEvent(getDatasetAtEvent(chart, event));
+    printElementAtEvent(getElementAtEvent(chart, event));
+    printElementsAtEvent(getElementsAtEvent(chart, event));
+  };
+
+  return (
+    <Chart
+      ref={chartRef}
+      type='bar'
+      onClick={onClick}
+      options={options}
+      data={data}
+    />
+  );
+}
+
+export default ChartEvents;
