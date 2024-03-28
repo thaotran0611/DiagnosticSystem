489f8de1b94a0f5e9160d6ed564f15012fa1ce5c
Thao <thao.tranthao0611@hcmut.edu.vn>
2024-03-29 01:37:02 +0700

    update backend

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from database import connect
 
 app = FastAPI()
+conn = connect()
+cursor = conn.cursor()
 
 origins = [
     "http://localhost:3000",
@@ -18,7 +20,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
+
+@app.get("/doctor", tags=["root"]) #test connect
+async def get_doctor() -> dict:
+    query = 'select * from doctor'
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return {"Doctor": result}
+
---
+++ backend/database.py
@@ -0,0 +1,21 @@
+
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
+
+# Connect to MySQL
+def connect():
+    return mysql.connector.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DB
+    )
