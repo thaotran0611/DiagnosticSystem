13ea97523410e8098f10397e59653290230876a7
Thao <thao.tranthao0611@hcmut.edu.vn>
2024-04-20 13:37:32 +0700

    commit

---
+++ frontend/src/components/MyTable/DiseaseTable.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react';
+import DiseaseCard from '../DiseaseCard/DiseaseCard';
+
+export default function DiseaseTable({ }) {
+  const data = [{'disease_code': 'AA', 'value': false, 'time': '2024-04-18 16:46:59'}
+              , {'disease_code': 'CND', 'value': false, 'time': '2024-04-18 16:46:59'}]
+  return (
+    <TableContainer>
+      <Table variant='simple'>
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th width='50%'>Disease</Th> {/* Set width for the Disease column */}
+            <Th width='25%'>%</Th> {/* Set width for the % column */}
+            <Th width='25%'>Diagnose</Th> {/* Set width for the Diagnose column */}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((row, index) => (
+            <Tr key={index}>
+              <Td width='50%'><DiseaseCard text={row.disease_code}/></Td> {/* Set width for the Disease column */}
+              <Td width='25%'>70% </Td> {/* Set width for the % column */}
+              <Td width='25%'>Pending</Td> {/* Set width for the Diagnose column */}
+            </Tr>
+          ))}
+        </Tbody>
+        {/* Tfoot section if needed */}
+      </Table>
+    </TableContainer>
+  );
+}
